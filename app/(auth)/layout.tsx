6f5b3387fff193639588c3236fcf8c3e443fa4ef
JH-devs <jitka.horavova01@gmail.com>
2024-01-16 17:18:36 +0100

    commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import BackgroundImage from "@/public/images/login_background.jpg";
+import Logo from "@/public/images/netflix_logo.svg";
+
+export default function AuthLayout({children} : {children:ReactNode}) {
+    return (
+        <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+            <Image 
+            src={BackgroundImage}
+            alt="background image"
+            className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+            priority
+            fill
+            />
+            <Image 
+            src={Logo}
+            alt="Logo Netflix"
+            width={120}
+            height={120}
+            priority
+            className="absolute left-4 top-4 object-contain md:left-10 md:top-6"
+            />
+            {children}
+        </div>
+    );
+};
